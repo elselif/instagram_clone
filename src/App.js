da5c1cc24b27d3a89cd01cd5e5a869221405410f
Elif <78614794+elselif@users.noreply.github.com>
2022-09-09 18:35:13 +0300

    --

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <>
-    instagram clone
-    </>
+    <div className="h-full w-full flex items-center justify-center">
+    <div className="w-[380px] h-[581px]">
+      <img className="w-[3 80px] h-[581px]" src=""/>
+    </div>
+    </div>
   );
 }
 
